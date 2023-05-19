605dcf89f091f857d0e1bb216bc00dfd89c52205
Marcela Roig <ilkumquat@gmail.com>
2023-05-19 16:49:32 -0300

    creación timeline, toogleen pagina donde comer y emparejamiento de estilos y páginas

--- Javascript/hamburguesa.js
+++ Javascript/hamburguesa.js
@@ -13,3 +13,25 @@ cerrar.addEventListener("click", () => {
     cerrar.classList.remove("visible")
     abrir.classList.remove("invisible")
 })
+
+const restoClick =  document.querySelector("#restoClick")
+const restoMenu = document.querySelector("#restoMenu")
+
+
+restoClick.addEventListener("click",()=>{
+    restoMenu.classList.toggle("visible")
+})
+const confiClick =  document.querySelector("#confiClick")
+const confiMenu = document.querySelector("#confiMenu")
+
+
+confiClick.addEventListener("click",()=>{
+    confiMenu.classList.toggle("visible")
+})
+const helaClick =  document.querySelector("#helaClick")
+const helaMenu = document.querySelector("#helaMenu")
+
+
+helaClick.addEventListener("click",()=>{
+    helaMenu.classList.toggle("visible")
+})
