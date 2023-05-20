8c2ff40603fe5a57222b51636d29cb8668376027
Laura Moyano <lauraymh000@hotmail.com>
2023-05-20 15:19:24 -0300

    Validación de formulario para revisar

--- Javascript/validacion.js
+++ Javascript/validacion.js
@@ -1,15 +1,17 @@
-
 function validarFormulario() {
+    var form = document.getElementById("form").value
     var nombre = document.getElementById("nombre").value.trim();
     var email = document.getElementById("email").value.trim();
     var descripcion = document.getElementById("descripcion").value.trim();
 
+   const enviarForm = (e) =>
+    e.preventDefault()
 
 
     if (nombre === "" || email === "" || descripcion === "") {
       alert("Por favor, complete todos los campos del formulario.");
       return false;
-    }
+    };
 
     
     for (var i = 0; i < nombre.length; i++) {
@@ -17,23 +19,24 @@ function validarFormulario() {
       if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32)) {
         alert("El campo 'nombre' solo puede contener letras y espacios.");
         return false;
-      }
-    }
+      };
+    };
 
-    if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
-      alert("La dirección de email " + valor + " es correcta");
-      return true;
-     } else {
+    
+    var emailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    
+    if ( !email.value.match(emailRegEx)) {
       alert("La dirección de email es incorrecta");
       return false;
-     }
-   
+      
+    }
+
       if (descripcion.length !== 500) {
         alert("El campo 'descripción' no debe superar los 500 caracteres.");
         return false;
-
-    }
-
+      };
+      
+  enviarForm
     alert("Formulario enviado correctamente.");
     return true;
-  }
+  };
